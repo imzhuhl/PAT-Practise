942a293e93d40fcdce29c1a8ae441c28fa850267
zhuhonglinX <zhuhl1996@gmail.com>
2018-02-14 21:28:31 +0800

    18

--- newcoder/18.cpp
+++ newcoder/18.cpp
@@ -4,27 +4,8 @@
 using namespace std;
 
 int main() {
-    string str;
-    cin >> str;
-
     int g1, g2, s1, s2, k1, k2;
-    int idx1 = 0, idx2 = 0;
-    idx1 = str.find(".");
-    g1 = atoi(str.substr(0, idx1).c_str());
-    str = str.substr(idx1 + 1, str.size());
-    idx2 = str.find(".");
-    s1 = atoi(str.substr(0, idx2).c_str());
-    str = str.substr(idx2 + 1, str.size());
-    k1 = atoi(str.c_str());
-
-    cin >> str;
-    idx1 = str.find(".");
-    g2 = atoi(str.substr(0, idx1).c_str());
-    str = str.substr(idx1 + 1, str.size());
-    idx2 = str.find(".");
-    s2 = atoi(str.substr(0, idx2).c_str());
-    str = str.substr(idx2 + 1, str.size());
-    k2 = atoi(str.c_str());
+    scanf("%d.%d.%d %d.%d.%d", &g1, &s1, &k1, &g2, &s2, &k2);
 
     int k = k1 + k2;
     int s = s1 + s2;
