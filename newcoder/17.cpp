101fd7632527e677ff0b737567ad488c179a31e9
zhuhonglinX <zhuhl1996@gmail.com>
2018-03-06 08:40:20 +0800

    17

--- newcoder/17.cpp
+++ newcoder/17.cpp
@@ -1,53 +1,61 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-int calculate(int valarr[], int &top) {
+int big[400][400];
+int bignum[400];
+int stack[100005];
+int top = 0;
+int N;
+
+int calculate(int &top) {
     int sum = 0;
-    for (int i = 0; i < 100005; i++) {
-        sum += valarr[i];
-        if (sum >= (top + 1) / 2) {
-            return i;
+    for (int i = 0; i < 400; i++) {
+        if (sum + bignum[i] < (top + 1) / 2)
+            sum += bignum[i];
+        else {
+            for (int j = 0; j < 400; j++) {
+                sum += big[i][j];
+                if (sum >= (top + 1) / 2) {
+                    return j + i * 400;
+                }
+            }
         }
     }
     return 0;
 }
 
 int main() {
-    int stack[100005];
-    int top = 0;
-    int valarr[100005];
-
-    int N;
-    cin >> N;
-
+    scanf("%d", &N);
     for (int i = 0; i < N; i++) {
-        string a;
-        cin >> a;
-        if (a == "Pop") {
+        char a[15];
+        scanf("%s", a);
+        if (strcmp(a, "Pop") == 0) {
             if (top == 0) {
                 printf("Invalid\n");
             } else {
                 int x = stack[top - 1];
                 top--;
-                valarr[x] -= 1;
+                big[x/400][x%400] -= 1;
+                bignum[x/400] -= 1;
                 printf("%d\n", x);
             }
-        } else if (a == "PeekMedian") {
+        } else if (strcmp(a, "PeekMedian") == 0) {
             if (top == 0) {
                 printf("Invalid\n");
             } else {
-                int v = calculate(valarr, top);
+                int v = calculate(top);
                 printf("%d\n", v);
             }
-        } else if (a == "Push") {
+        } else if (strcmp(a, "Push") == 0) {
             int x;
-            cin >> x;
+            scanf("%d", &x);
             stack[top++] = x;
-            valarr[x] += 1;
+            big[x/400][x%400] += 1;
+            bignum[x/400] += 1;
         }
     }
 
