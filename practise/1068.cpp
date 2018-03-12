70f0d8e73453d53f98d34fc539c17b09626ed2a0
zhuhonglinX <zhuhl1996@gmail.com>
2018-03-12 11:59:34 +0800

    1068

---
+++ practise/1068.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m;
+    int coin[10001];
+    int dp[10001][101] = {0};
+    int check[10001][101] = {0};
+    scanf("%d%d", &n, &m);
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &coin[i]);
+    }
+    sort(coin + 1, coin + n + 1, greater<int>());
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = coin[i]; j <= m; j++) {
+            if (dp[i - 1][j] <= dp[i - 1][j - coin[i]] + coin[i]) {
+                dp[i][j] = dp[i - 1][j - coin[i]] + coin[i];
+                check[i][j] = coin[i];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+                check[i][j] = check[i - 1][j];
+            }
+        }
+        
+    }
+
+    if (dp[n][m] == m) {
+        int sum = 0;
+        int p = m;
+        int r = n;
+        while (sum < m) {
+            int x = check[r][p];
+            printf("%d", x);
+            p = p - x;
+            sum += x;
+            r--;
+            if (sum < m) printf(" ");
+        }
+        printf("\n");
+    } else {
+        printf("No Solution\n");
+    }
+
+    return 0;
+}
