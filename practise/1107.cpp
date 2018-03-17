45650f4803127c2770fa079d5a4d1b6aeb5cb545
zhuhonglinX <zhuhl1996@gmail.com>
2018-03-17 13:16:39 +0800

    rewrite 1107

--- practise/1107.cpp
+++ practise/1107.cpp
@@ -1,65 +1,58 @@
 #include <iostream>
-#include <map>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int N;
-int course[1001] = {0};
-vector<int> father;
+int father[1001];
+int people[1001];
+int n;
 
 int Find(int a) {
-    while (a != father[a]) {
+    while (father[a] != a) {
         father[a] = father[father[a]];
         a = father[a];
     }
     return a;
-
 }
+
 void Union(int a, int b) {
-    a = Find(a);
-    b = Find(b);
-    if (a != b)
-        father[a] = b;
-}
-
-bool cmp(int &a, int &b) {
-    return a > b;
+    int fa = Find(a);
+    int fb = Find(b);
+    if (fa != fb) {
+        father[fa] = fb;
+    }
 }
 
 int main() {
-    scanf("%d", &N);
-    father.resize(N + 1);
-    for (int i = 0; i <= N; i++) {
+    scanf("%d", &n);
+    for (int i = 1; i <= 1000; i++) {
         father[i] = i;
     }
-    for (int i = 1; i <= N; i++) {
-        int hobnum;
-        scanf("%d:", &hobnum);
-        for (int j = 0; j < hobnum; j++) {
-            int x;
-            scanf("%d", &x);
-            if (course[x] == 0) {
-                course[x] = i;
-            }
-            Union(i, course[x]);
+    for (int i = 1; i <= n; i++) {
+        int v, id;
+        scanf("%d: ", &v);
+        scanf("%d", &id);
+        people[i] = id;
+        for (int j = 1; j < v; j++) {
+            int oid;
+            scanf("%d", &oid);
+            Union(id, oid);
         }
     }
 
-    int clu[1005] = {0};
-    int count = 0;
-    for (int i = 1; i <= N; i++) {
-        int x = Find(i);
-        if (clu[x] == 0) count++;
-        clu[x]++;
+    int visit[1001] = {0};
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        int x = Find(people[i]);
+        if (visit[x] == 0) cnt++;
+        visit[x]++;
     }
-    sort(clu, clu + 1001, cmp);
-    printf("%d\n", count);
-    for (int i = 0; i < count; i++) {
+
+    sort(visit, visit + 1001, greater<int>());
+    printf("%d\n", cnt);
+    for (int i = 0; i < cnt; i++) {
         if (i) printf(" ");
-        printf("%d", clu[i]);
+        printf("%d", visit[i]);
     }
     printf("\n");
-
     return 0;
 }
